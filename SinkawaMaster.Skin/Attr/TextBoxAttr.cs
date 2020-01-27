9ec1b46b21f86a1cf75b3dad87904d94a57a4c8b
Anibal Juniper <redoubteifly@outlook.com>
2020-01-27 12:11:04 +0800

    upd

---
+++ SinkawaMaster.Skin/Attr/TextBoxAttr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SinkawaMaster.Skin.Attr
+{
+    public class TextBoxAttr : TextBox
+    {
+
+        public static readonly DependencyProperty MaxlenDependency =
+            DependencyProperty.Register("Maxlen", typeof(int), typeof(TextBoxAttr), new PropertyMetadata(""));
+
+        public int Maxlen
+        {
+            get { return (int)GetValue(MaxlenDependency); }
+            set { SetValue(MaxlenDependency, value); }
+        }
+
+
+    }
+}

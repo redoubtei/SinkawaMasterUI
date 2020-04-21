00af24ec911e29db3777ba5ea1ee91804a209617
Anibal Juniper <redoubteifly@outlook.com>
2020-04-21 22:00:38 +0800

    update

--- SinkawaMaster.Client.App/Window1.xaml.cs
+++ SinkawaMaster.Client.App/Window1.xaml.cs
@@ -17,7 +17,7 @@ namespace SinkawaMaster.Client.App
     /// <summary>
     /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Window1 
     {
         public Window1()
         {
